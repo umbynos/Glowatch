beefb8f4ddd0ba51e3efbe03a7d8d4455c98927f
Umberto Baldi <umberto.baldi@edu.unito.it>
2018-11-23 22:06:57 +0100

    added rainbow effect on boot and LDR

--- Glowatch.ino
+++ Glowatch.ino
@@ -1,7 +1,8 @@
 #include <RTCZero.h>
 #include <Adafruit_NeoPixel.h>
 
-#define LED_PIN 7
+#define LDR_PIN A0
+#define LED_PIN 5
 #define NPIXELS 60
 #define LED_BRIGHTNESS 64
 
@@ -16,6 +17,7 @@ void setup() {
   rtc.setMinutes((__TIME__[3] - '0') * 10 + __TIME__[4] - '0');
   rtc.setSeconds((__TIME__[6] - '0') * 10 + __TIME__[7] - '0');
   strip.begin();
+  rainbowCycle(20);
   strip.setBrightness(LED_BRIGHTNESS);
   strip.show();
 }
@@ -51,6 +53,8 @@ void debugPrint(){
   Serial.print(":");
   print2digits(rtc.getSeconds());
   Serial.println();
+  Serial.println(analogRead(LDR_PIN));
+  //low light 300
 }
 
 void print2digits(int number) {
@@ -70,14 +74,26 @@ void getTime() {
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
+//divider reduces brightness by half every increment
 uint32_t Wheel(byte WheelPos, byte divider) {
   if(WheelPos < 85) {
-   return strip.Color((WheelPos * 3>>divider), (255 - WheelPos * 3)>>divider, 0);
+   return strip.Color((WheelPos * 3>>divider), (255 - WheelPos * 3)>>divider, 0);//R-G
   } else if(WheelPos < 170) {
    WheelPos -= 85;
-   return strip.Color((255 - WheelPos * 3)>>divider, 0, (WheelPos * 3)>>divider);
+   return strip.Color((255 - WheelPos * 3)>>divider, 0, (WheelPos * 3)>>divider);//-RB
   } else {
    WheelPos -= 170;
-   return strip.Color(0, (WheelPos * 3)>>divider, (255 - WheelPos * 3)>>divider);
+   return strip.Color(0, (WheelPos * 3)>>divider, (255 - WheelPos * 3)>>divider);//G-B
+  }
+}
+  void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*3; j++) { // 3 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255,0));
+    }
+    strip.show();
+    delay(wait);
   }
 }
